85d7559385b4b4e29049b2d87d86565befa8df24
Alex Portell <amportell.13@gmail.com>
2024-02-29 03:53:38 -0500

    Added logic to update items in a Collection (comes with menu strip in Main Form) or List (items that are not present in Collection).

--- VACM/VACM.GUI.NET4_0/ViewModels/FormColorUpdater.cs
+++ VACM/VACM.GUI.NET4_0/ViewModels/FormColorUpdater.cs
@@ -44,5 +44,152 @@ namespace VACM.GUI.NET4_0.ViewModels
         }
 
         #endregion
+
+        #region Logic
+
+        /// <summary>
+        /// Set the colors of every control in list, given dark mode is enabled or not.
+        /// </summary>
+        /// <param name="controlList">The control list</param>
+        public static void SetColorsOfControlList(List<Control> controlList)
+        {
+            if (controlList.Count == 0)
+            {
+                return;
+            }
+
+            foreach (Control control in controlList)
+            {
+                control.BackColor = BackColor;
+                control.ForeColor = ForeColor;
+                SetColorsOfControlCollection(control.Controls);
+            };
+        }
+
+        /// <summary>
+        /// Set the colors of the tool strip item, given dark mode is enabled or not.
+        /// </summary>
+        /// <param name="toolStripItem">The tool strip item</param>
+        internal static void SetColorsOfToolStripItem
+            (ToolStripItem toolStripItem)
+        {
+            toolStripItem.BackColor = BackColor;
+            toolStripItem.ForeColor = ForeColor;
+
+            if (toolStripItem is ToolStripMenuItem)
+            {
+                SetColorsOfToolStripItem(toolStripItem as ToolStripMenuItem);
+            }
+
+        }
+
+        /// <summary>
+        /// Set the colors of the tool strip menu item and its tool strip item
+        /// collection, given dark mode is enabled or not.
+        /// </summary>
+        /// <param name="toolStripMenuItem">The tool strip menu item</param>
+        internal static void SetColorsOfToolStripItem
+            (ToolStripMenuItem toolStripMenuItem)
+        {
+            toolStripMenuItem.BackColor = BackColor;
+            toolStripMenuItem.ForeColor = ForeColor;
+
+            if (toolStripMenuItem.DropDownItems.Count == 0)
+            {
+                return;
+            }
+
+            foreach (var item in
+                toolStripMenuItem.DropDownItems)
+            {
+                SetColorsOfToolStripItem(item as ToolStripItem);
+            }
+        }
+
+        /// <summary>
+        /// Set the colors of every item in tool strip item collection, 
+        /// given dark mode is enabled or not.
+        /// </summary>
+        /// <param name="toolStripItemCollection">The tool strip item collection</param>
+        internal static void SetColorsOfToolStripItemCollection
+            (ToolStripItemCollection toolStripItemCollection)
+        {
+            foreach (ToolStripItem toolStripItem in toolStripItemCollection)
+            {
+                SetColorsOfToolStripItem(toolStripItem);
+            }
+        }
+
+        /// <summary>
+        /// Set the colors of the constructor, given dark mode is enabled or not.
+        /// </summary>
+        /// <param name="parentObject">The parent object</param>
+        public static void SetColorsOfConstructor(object parentObject)
+        {
+            if (!(parentObject is Form))
+            {
+                return;
+            }
+
+            (parentObject as Form).BackColor = BackColor;
+            (parentObject as Form).ForeColor = ForeColor;
+        }
+
+        /// <summary>
+        /// Set the colors of every control, given dark mode is enabled or not.
+        /// </summary>
+        /// <param name="controlCollection">The control collection</param>
+        public static void SetColorsOfControlCollection
+            (Control.ControlCollection controlCollection)
+        {
+            if (controlCollection.Count == 0)
+            {
+                return;
+            }
+
+            foreach (var control in controlCollection)
+            {
+                (control as Control).BackColor = BackColor;
+                (control as Control).ForeColor = ForeColor;
+
+                if (control is Control.ControlCollection)
+                {
+                    SetColorsOfControlCollection
+                        (control as Control.ControlCollection);
+                }
+
+                SetColorsOfControlCollection((control as Control).Controls);
+            }
+        }
+
+        /// <summary>
+        /// Set the colors of every tool strip item in list, 
+        /// given dark mode is enabled or not.
+        /// </summary>
+        /// <param name="toolStripItemList">The tool strip item list</param>
+        public static void SetColorsOfToolStripItemList
+            (List<ToolStripItem> toolStripItemList)
+        {
+            toolStripItemList.ForEach(toolStripItem =>
+            {
+                SetColorsOfToolStripItem(toolStripItem);
+            });
+        }
+
+        /// <summary>
+        /// Set the colors of every tool strip menu item in list, 
+        /// given dark mode is enabled or not.
+        /// </summary>
+        /// <param name="toolStripMenuItemList">The tool strip menu item list</param>
+        public static void SetColorsOfToolStripItemList
+            (List<ToolStripMenuItem> toolStripMenuItemList)
+        {
+            toolStripMenuItemList.ForEach(toolStripMenuItem =>
+            {
+                SetColorsOfToolStripItem(toolStripMenuItem);
+            });
+        }
+
+        #endregion
     }
 }
