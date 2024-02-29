d9c116a4bcaf3ef1ce5c0ef49ba15723ea59cbee
Alex Portell <amportell.13@gmail.com>
2024-02-28 23:40:23 -0500

    Added methods to populate dictionaries. Formatted methods by 80/24 rule. Added getter for list in dict, added windows identity for circumstance for the hive HKEY_USERS (HKEY_CURRENT_USER is not compatible with Registry event WQL queries).

--- VACM.GUI.NET4_0/VACM.GUI.NET4_0/Extensions/WMIRegistryEventListener.cs
+++ VACM.GUI.NET4_0/VACM.GUI.NET4_0/Extensions/WMIRegistryEventListener.cs
@@ -13,11 +13,28 @@ namespace VACM.NET4.Extensions
     {
         #region Parameters
 
+        private WindowsIdentity currentWindowsIdentity;
+
+        private List<ManagementEventWatcher> managementEventWatcherList
+        {
+            get
+            {
+                return registryKeyPathAndValueNameAndManagementEventWatcherDictionary
+                    .SelectMany(registryValueNameAndManagementEventWatcherDictionary =>
+                        registryValueNameAndManagementEventWatcherDictionary.Value
+                        .Select(managementEventWatcher => managementEventWatcher.Value))
+                        .ToList();
+            }
+        }
+
         private Dictionary<string, Dictionary<string, ManagementEventWatcher>>
             registryKeyPathAndValueNameAndManagementEventWatcherDictionary;
 
         private Dictionary<RegistryHive, Dictionary<string, List<string>>>
             registryHiveAndKeyPathAndValueNameListDictionary;
+
+        private Dictionary<RegistryHive, Dictionary<string, Dictionary<string, string>>>
+            registryHiveAndKeyPathAndValueNameAndSubKeyValueDictionary;
 
         /// <summary>
         /// Valid hive objects and values for RegistryEvent class and derivatives.
@@ -121,6 +138,28 @@ namespace VACM.NET4.Extensions
         }
 
         /// <summary>
+        /// Get the management event watcher.
+        /// </summary>
+        /// <param name="query">The query</param>
+        /// <returns>The management event watcher</returns>
+        internal ManagementEventWatcher GetManagementEventWatcher(string query)
+        {
+            ManagementEventWatcher managementEventWatcher =
+                new ManagementEventWatcher(new EventQuery(query));
+
+            int timeoutInSeconds = 5;
+
+            managementEventWatcher.Options.Timeout =
+                TimeSpan.FromSeconds(timeoutInSeconds);
+
+            managementEventWatcher.EventArrived +=
+                new EventArrivedEventHandler
+                    (RegistryEventHandler_GetSubKeyValueOnRegistryValueChangeEvent);
+
+            return managementEventWatcher;
+        }
+
+        /// <summary>
         /// Get the registry key from the valid hive.
         /// </summary>
         /// <param name="registryHive">The registry hive</param>
@@ -143,6 +182,189 @@ namespace VACM.NET4.Extensions
                 default:
                     return null;
             }
+        }
+
+        /// <summary>
+        /// Get the WQL query as a string.
+        /// </summary>
+        /// <param name="registryHive">The registry hive</param>
+        /// <param name="registryKeyPath">The registry key path</param>
+        /// <param name="registryValueName">The registry value name</param>
+        /// <returns>The WQL query</returns>
+        internal string GetQuery(RegistryHive registryHive, string registryKeyPath,
+            string registryValueName)
+        {
+            if (string.IsNullOrWhiteSpace(registryKeyPath)
+                || string.IsNullOrWhiteSpace(registryValueName))
+            {
+                return string.Empty;
+            }
+
+            string registryHiveAsString =
+                validRegistryHiveObjectAndValueDictionary[registryHive];
+
+            if (registryHive == RegistryHive.Users)
+            {
+                registryKeyPath = string.Format("{0}\\{1}",
+                    currentWindowsIdentity.User.Value, registryKeyPath);
+
+                registryKeyPath = registryKeyPath.Replace("\\", "\\\\");
+            }
+
+            string table = "RegistryValueChangeEvent";
+
+            return string.Format
+                ("{0} {1} {2} {3} {4}",
+                "SELECT *",
+                $"FROM {table}",
+                $"WHERE Hive = '{registryHiveAsString}'",
+                $@"AND KeyPath = '{registryKeyPath}'",
+                $"AND ValueName = '{registryValueName}'");
+        }
+
+        /// <summary>
+        /// Parse constructor dictionary and set dictionaries.
+        /// </summary>
+        internal void ParseConstructorDictionaryAndSetDictionaries()
+        {
+            registryHiveAndKeyPathAndValueNameListDictionary.Keys.ToList().ForEach
+                (registryHive => ParseRegistryHiveAndSetDictionaries(registryHive));
+        }
+
+        /// <summary>
+        /// Parse registry hive and set dictionaries.
+        /// </summary>
+        /// <param name="registryHive">The registry hive</param>
+        internal void ParseRegistryHiveAndSetDictionaries(RegistryHive registryHive)
+        {
+            registryHiveAndKeyPathAndValueNameListDictionary[registryHive].Keys.ToList()
+                .ForEach
+                    (registryKeyPath => ParseRegistryHiveAndKeyPathAndSetDictionaries
+                        (registryHive, registryKeyPath));
+        }
+
+        /// <summary>
+        /// Parse registry hive and registry 
+        /// </summary>
+        /// <param name="registryHive">The registry hive</param>
+        /// <param name="registryKeyPath">The registry key path</param>
+        internal void ParseRegistryHiveAndKeyPathAndSetDictionaries
+            (RegistryHive registryHive, string registryKeyPath)
+        {
+            registryHiveAndKeyPathAndValueNameListDictionary
+                [registryHive][registryKeyPath].ForEach(registryValueName =>
+                ParseRegistryHiveAndKeyPathAndValueNameAndSetDictionaries
+                    (registryHive, registryKeyPath, registryValueName));
+        }
+
+        /// <summary>
+        /// Parse registry key path and set dictionaries.
+        /// </summary>
+        /// <param name="registryHive">The registry hive</param>
+        /// <param name="registryKeyPath">The registry key path</param>
+        /// <param name="registryValueName">The registry value name</param>
+        internal void ParseRegistryHiveAndKeyPathAndValueNameAndSetDictionaries
+            (RegistryHive registryHive, string registryKeyPath,
+            string registryValueName)
+        {
+            if (string.IsNullOrWhiteSpace(registryValueName))
+            {
+                return;
+            }
+
+            if (!validRegistryHiveObjectAndValueDictionary
+                .ContainsKey(registryHive))
+            {
+                string message = $"Registry Hive '{registryHive.ToString()} is not" +
+                    "supported.'";
+
+                throw new ArgumentException(message);
+            }
+
+            string query =
+                GetQuery(registryHive, registryKeyPath, registryValueName);
+
+            ManagementEventWatcher managementEventWatcher =
+                GetManagementEventWatcher(query);
+
+            if (!registryKeyPathAndValueNameAndManagementEventWatcherDictionary
+                .ContainsKey(registryKeyPath))
+            {
+                registryKeyPathAndValueNameAndManagementEventWatcherDictionary.Add
+                    (registryKeyPath,
+                    new Dictionary<string, ManagementEventWatcher>()
+                        {
+                            { registryValueName, managementEventWatcher },
+                        });
+            }
+            else
+            {
+                registryKeyPathAndValueNameAndManagementEventWatcherDictionary
+                    [registryKeyPath].Add(registryValueName, managementEventWatcher);
+            }
+
+            //TODO: set value dict here.
+        }
+
+        /// <summary>
+        /// On RegistryValueChangeEvent, extract the WQL query fields related to the
+        /// dictionary, and get the current sub key value.
+        /// </summary>
+        /// <param name="sender">The sender object</param>
+        /// <param name="eventArrivedEventArgs">The event arrived event arguments
+        /// </param>
+        internal void RegistryEventHandler_GetSubKeyValueOnRegistryValueChangeEvent
+            (object sender, EventArrivedEventArgs eventArrivedEventArgs)
+        {
+            if (!(sender is ManagementEventWatcher))
+            {
+                return;
+            }
+
+            string registryHiveAsString = null;
+            string registryKeyPath = null;
+            string registryValueName = null;
+
+            foreach (var propertyData in eventArrivedEventArgs.NewEvent.Properties)
+            {
+                switch (propertyData.Name)
+                {
+                    case "Hive":
+                        registryHiveAsString = propertyData.Value.ToString();
+                        continue;
+
+                    case "KeyPath":
+                        registryKeyPath = propertyData.Value.ToString();
+                        continue;
+
+                    case "ValueName":
+                        registryValueName = propertyData.Value.ToString();
+                        continue;
+
+                    default:
+                        continue;
+                }
+            }
+
+            if (string.IsNullOrWhiteSpace(registryHiveAsString)
+                || string.IsNullOrWhiteSpace(registryKeyPath)
+                || string.IsNullOrWhiteSpace(registryValueName))
+            {
+                return;
+            }
+
+            RegistryHive registryHive = validRegistryHiveObjectAndValueDictionary
+                .FirstOrDefault(x => x.Value == registryHiveAsString).Key;
+
+            SetSubKeyValueInDictionary
+                (registryHive, registryKeyPath, registryValueName);
+
+            //TODO:
+            /* 
+             * -when the actual subkey value changes, how do I tell this class to
+             * update the relevant boolean value in another class?
+             * 
+             */
         }
 
         #endregion
