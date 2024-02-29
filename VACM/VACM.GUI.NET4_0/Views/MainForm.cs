4f59bfe67ec928cb9fc79b382b94fce29a40a9e2
Alex Portell <amportell.13@gmail.com>
2024-02-29 03:34:45 -0500

    Added models as constructor objects.

--- VACM/VACM.GUI.NET4_0/Views/MainForm.cs
+++ VACM/VACM.GUI.NET4_0/Views/MainForm.cs
@@ -7,12 +7,21 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using VACM.GUI.NET4_0.Models;
+using VACM.GUI.NET4_0.ViewModels;
+using VACM.NET4.Models;
 
 namespace VACM.GUI.NET4_0.Views
 {
     public partial class MainForm : Form
     {
         #region Parameters
+
+        private string fileName;
+        private DeviceListModel deviceListModel;
+        private DeviceControl inputDeviceControl { get; set; }
+        private DeviceControl outputDeviceControl { get; set; }
+        private RepeaterDataModel repeaterDataModel { get; set; }
 
         public const string WaveInAsString = "Wave In";
         public const string WaveOutAsString = "Wave Out";
