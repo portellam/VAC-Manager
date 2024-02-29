15b425aadfc138f925006b420971c17a63ff6b39
Alex Portell <amportell.13@gmail.com>
2024-02-28 20:53:43 -0500

    Removed line for deleted form.

--- VACM.GUI.NET4_0/VACM.GUI.NET4_0/Program.cs
+++ VACM.GUI.NET4_0/VACM.GUI.NET4_0/Program.cs
@@ -15,7 +15,7 @@ namespace VACM.GUI.NET4_0
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            //TODO: add new form.
         }
     }
 }
