3e71d14b978d01943947b3bf2881d059f6029be9
Alex Portell <amportell.13@gmail.com>
2024-02-28 23:12:36 -0500

    Added TODO, created lists, and two constructors.

--- VACM.GUI.NET4_0/VACM.GUI.NET4_0/Models/DeviceListModel.cs
+++ VACM.GUI.NET4_0/VACM.GUI.NET4_0/Models/DeviceListModel.cs
@@ -1,17 +1,78 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
-using System.Text;
+using NAudio.CoreAudioApi;
 
-namespace VACM.GUI.NET4_0.Models
+namespace VACM.NET4.Models
 {
     public class DeviceListModel
     {
+        /*
+         * -create constructor helper logic.
+         * -set and sort device lists.
+         * -set unselected lists.
+         * -???
+         * 
+         */
+
         #region Parameters
+
+        public List<MMDevice> AllDeviceList { get; private set; }
+        public List<MMDevice> AllWaveInDeviceList { get; private set; }
+        public List<MMDevice> AllWaveOutDeviceList { get; private set; }
+
+        public List<MMDevice> SelectedWaveInMMDeviceList { get; private set; }
+        public List<MMDevice> SelectedWaveOutMMDeviceList { get; private set; }
+
+        public List<MMDevice> UnselectedWaveInMMDeviceList { get; private set; }
+        public List<MMDevice> UnselectedWaveOutMMDeviceList { get; private set; }
+
+        public List<string> AllWaveInNameList { get; private set; }
+        public List<string> AllWaveOutNameList { get; private set; }
+
+        public List<string> SelectedWaveInNameList { get; private set; }
+        public List<string> SelectedWaveOutNameList { get; private set; }
+
+        public List<string> UnselectedWaveInNameList { get; private set; }
+        public List<string> UnselectedWaveOutNameList { get; private set; }
+
+        public static DeviceState Present =
+            DeviceState.Active | DeviceState.Unplugged | DeviceState.Disabled;
 
         #endregion
 
         #region Logic
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        [ExcludeFromCodeCoverage]
+        public DeviceListModel()
+        {
+            AllDeviceList = new MMDeviceEnumerator()
+                .EnumerateAudioEndPoints(DataFlow.All, Present).Distinct().ToList();
+
+            SelectedWaveInMMDeviceList = new List<MMDevice>();
+            SelectedWaveInNameList = new List<string>();
+            SelectedWaveOutMMDeviceList = new List<MMDevice>();
+            SelectedWaveOutNameList = new List<string>();
+        }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="allDeviceList">The MMDevice list</param>
+        public DeviceListModel(List<MMDevice> allDeviceList)
+        {
+            AllDeviceList = allDeviceList.Where
+                (mMDevice => mMDevice.State == Present).Distinct().ToList();
+
+            SelectedWaveInMMDeviceList = new List<MMDevice>();
+            SelectedWaveInNameList = new List<string>();
+            SelectedWaveOutMMDeviceList = new List<MMDevice>();
+            SelectedWaveOutNameList = new List<string>();
+        }
 
         #endregion
     }
