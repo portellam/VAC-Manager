f4200dd7f63d2d2f4924b0f199721922f3436855
Alex Portell <amportell.13@gmail.com>
2024-02-29 03:51:27 -0500

    Create FormColorUpdater.cs

---
+++ VACM/VACM.GUI.NET4_0/ViewModels/FormColorUpdater.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VACM.GUI.NET4_0.ViewModels
+{
+    internal class FormColorUpdater
+    {
+    }
+}
