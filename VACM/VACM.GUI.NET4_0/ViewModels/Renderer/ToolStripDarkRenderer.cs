3460eb2448967951c20cb12a62c7ef305b5ec96c
Alex Portell <amportell.13@gmail.com>
2024-02-29 03:43:20 -0500

    Added getter to create renderer with dark color table.

--- VACM/VACM.GUI.NET4_0/ViewModels/Renderer/ToolStripDarkRenderer.cs
+++ VACM/VACM.GUI.NET4_0/ViewModels/Renderer/ToolStripDarkRenderer.cs
@@ -1,8 +1,25 @@
 ﻿using System.Windows.Forms;
+using VACM.GUI.NET4_0.ViewModels.ColorTable;
 
 namespace VACM.GUI.NET4_0.ViewModels.Renderer
 {
     public class ToolStripDarkRenderer
     {
+        #region Parameters
+
+        private readonly DarkColorTable darkColorTable = new DarkColorTable();
+
+        public ToolStripProfessionalRenderer ToolStripProfessionalRenderer =>
+            new ToolStripProfessionalRenderer(darkColorTable);
+
+        public new ProfessionalColorTable ColorTable
+        {
+            get
+            {
+                return darkColorTable;
+            }
+        }
+
+        #endregion
     }
 }
