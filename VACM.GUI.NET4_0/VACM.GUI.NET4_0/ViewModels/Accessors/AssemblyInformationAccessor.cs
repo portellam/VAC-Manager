66a5e648c687591dfb7fdfe9c526aac77d07b5fe
Alex Portell <amportell.13@gmail.com>
2024-02-28 22:56:01 -0500

    Added assembly information accessor, TODO: carry over logic from about form.

---
+++ VACM.GUI.NET4_0/VACM.GUI.NET4_0/ViewModels/Accessors/AssemblyInformationAccessor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VACM.GUI.NET4_0.ViewModels.Accessors
+{
+    public class AssemblyInformationAccessor
+    {
+        #region Logic
+
+        #endregion
+    }
+}
