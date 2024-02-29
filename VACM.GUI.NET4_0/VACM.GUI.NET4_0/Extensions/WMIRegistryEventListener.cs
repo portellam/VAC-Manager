e30345ba74e17627df012fc7cb51d5dccf9f69e6
Alex Portell <amportell.13@gmail.com>
2024-02-28 23:30:11 -0500

    Added TODO. Added constructor and began work on dicts.

--- VACM.GUI.NET4_0/VACM.GUI.NET4_0/Extensions/WMIRegistryEventListener.cs
+++ VACM.GUI.NET4_0/VACM.GUI.NET4_0/Extensions/WMIRegistryEventListener.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Management;
 using System.Security.Principal;
 using System.Threading;
 using System.Threading.Tasks;
@@ -10,6 +11,66 @@ namespace VACM.NET4.Extensions
 {
     public class WMIRegistryEventListener
     {
+        #region Parameters
+
+        private Dictionary<string, Dictionary<string, ManagementEventWatcher>>
+            registryKeyPathAndValueNameAndManagementEventWatcherDictionary;
+
+        private Dictionary<RegistryHive, Dictionary<string, List<string>>>
+            registryHiveAndKeyPathAndValueNameListDictionary;
+
+        /// <summary>
+        /// Valid hive objects and values for RegistryEvent class and derivatives.
+        /// URL: https://learn.microsoft.com/en-us/previous-versions/windows/desktop/regprov/registrykeychangeevent
+        /// </summary>
+        private static Dictionary<RegistryHive, string>
+            validRegistryHiveObjectAndValueDictionary =
+            new Dictionary<RegistryHive, string>
+            {
+                {
+                    RegistryHive.LocalMachine,      "HKEY_LOCAL_MACHINE"
+                },
+                {
+                    RegistryHive.Users,             "HKEY_USERS"
+                },
+            };
+
+        #endregion
+
+        #region Constructor Logic
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="registryHive">The registry hive</param>
+        /// <param name="registryKeyPath">The registry key path</param>
+        /// <param name="registryValueNameList">The registry value name list</param>
+        public WMIRegistryEventListener(RegistryHive registryHive,
+            string registryKeyPath, List<string> registryValueNameList)
+        {
+            this.registryHiveAndKeyPathAndValueNameListDictionary =
+                new Dictionary<RegistryHive, Dictionary<string, List<string>>>()
+                {
+                    {
+                        registryHive, new Dictionary<string, List<string>>
+                        {
+                            { registryKeyPath, registryValueNameList }
+                        }
+                    },
+                };
+
+            //TODO: add constuctor helper here.
+        }
+
+        //TODO: add multiple constructors for ease of use.
+
+        //TODO: add start and stop watcher methods.
+
+        //TODO: add dispose.
         
+        // TODO: add on value change event.
+
+
+        #endregion
     }
 }
