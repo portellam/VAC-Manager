ad0f243337ce48826e3dc5c47111aca4b62be4b8
Alex Portell <amportell.13@gmail.com>
2024-02-29 00:39:22 -0500

    Added strings for file interaction logic.

--- VACM/VACM.GUI.NET4_0/Common.cs
+++ VACM/VACM.GUI.NET4_0/Common.cs
@@ -7,5 +7,17 @@ namespace VACM.GUI.NET4_0
 {
     public class Common
     {
+        #region Application name logic
+
+        public readonly static string ApplicationNameAsAbbreviation = "VACM";
+
+        public readonly static string FileExtension = "." +
+            ApplicationNameAsAbbreviation.ToLower();
+
+        public readonly static string ReferencedApplicationName = "Virtual Audio Cable";
+        public readonly static string ReferencedFileExtension = ".vac";
+
+        #endregion
+
     }
 }
