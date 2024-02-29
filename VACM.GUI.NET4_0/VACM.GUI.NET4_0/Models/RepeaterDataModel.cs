ae4406a81150c99978cf75fd29d0da430eac7f1a
Alex Portell <amportell.13@gmail.com>
2024-02-28 22:55:23 -0500

    Added repeater data model.

---
+++ VACM.GUI.NET4_0/VACM.GUI.NET4_0/Models/RepeaterDataModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VACM.GUI.NET4_0.Models
+{
+    public class RepeaterDataModel
+    {
+        #region Parameters
+
+        #endregion
+
+        #region Logic
+
+        #endregion
+    }
+}
