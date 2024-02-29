cd98f3ba8e90fa7c2b392e94ed5065645b8962a6
Alex Portell <amportell.13@gmail.com>
2024-02-28 20:54:09 -0500

    Changed modifier.

--- VACM.GUI.NET4_0/VACM.GUI.NET4_0/Program.cs
+++ VACM.GUI.NET4_0/VACM.GUI.NET4_0/Program.cs
@@ -5,7 +5,7 @@ using System.Windows.Forms;
 
 namespace VACM.GUI.NET4_0
 {
-    internal static class Program
+    internal class Program
     {
         /// <summary>
         /// The main entry point for the application.
