d0ba017c857d057ad240a1002af80d65e4bcada2
Alex Portell <amportell.13@gmail.com>
2024-02-29 00:21:59 -0500

    Added DeviceControl for GUI grid.

---
+++ VACM/VACM.GUI.NET4_0/ViewModels/DeviceControl.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace VACM.GUI.NET4_0.ViewModels
+{
+    public partial class DeviceControl : Control
+    {
+        public DeviceControl()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnPaint(PaintEventArgs pe)
+        {
+            base.OnPaint(pe);
+        }
+    }
+}
