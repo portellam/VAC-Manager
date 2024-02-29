689737240c942f35d982bfce23de0af9129a7057
Alex Portell <amportell.13@gmail.com>
2024-02-29 00:33:09 -0500

    Create LightThemeValidator.cs

---
+++ VACM/VACM.GUI.NET4_0/ViewModels/LightThemeValidator.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using VACM.NET4.Extensions;
+
+namespace VACM.GUI.NET4_0.ViewModels
+{
+    public class LightThemeValidator
+    {
+
+    }
+}
