676e86b9a0a2676a254c38f63d0d2f75a0c5b44d
Alex Portell <amportell.13@gmail.com>
2024-02-29 03:02:44 -0500

    Added logic to populate dictionary.

--- VACM/VACM.GUI.NET4_0/Models/RepeaterDataModel.cs
+++ VACM/VACM.GUI.NET4_0/Models/RepeaterDataModel.cs
@@ -27,6 +27,56 @@ namespace VACM.GUI.NET4_0.Models
                 Dictionary<DeviceControl, RepeaterModel>>();
         }
 
+        /// <summary>
+        /// Adds a dictionary to the repeater data model, if it does not already exist.
+        /// </summary>
+        /// <param name="firstDeviceControl">The first device control</param>
+        /// <param name="secondDeviceControl">The second device control</param>
+        public void AddDictionary
+            (DeviceControl firstDeviceControl, DeviceControl secondDeviceControl)
+        {
+            AddThisDictionary(firstDeviceControl, secondDeviceControl);
+            AddThisDictionary(secondDeviceControl, firstDeviceControl);
+        }
+
+        /// <summary>
+        /// Adds a repeater to the repeater data model, if it does not already exist.
+        /// </summary>
+        /// <param name="firstDeviceControl">The first device control</param>
+        /// <param name="secondDeviceControl">The second device control</param>
+        public void AddThisDictionary
+            (DeviceControl inputDeviceControl, DeviceControl outputDeviceControl)
+        {
+            if (inputDeviceControl is null || outputDeviceControl is null
+                || (RepeaterData.ContainsKey(inputDeviceControl)
+                    && RepeaterData[inputDeviceControl]
+                        .ContainsKey(outputDeviceControl)))
+            {
+                return;
+            }
+
+            RepeaterData.Add(inputDeviceControl,
+                GetDictionary(inputDeviceControl, outputDeviceControl));
+        }
+
+        /// <summary>
+        /// Return a valid dictionary object.
+        /// </summary>
+        /// <param name="firstDeviceControl">The first device control</param>
+        /// <param name="secondDeviceControl">The second device control</param>
+        /// <returns>The dictionary object</returns>
+        internal Dictionary<DeviceControl, RepeaterModel> GetDictionary
+            (DeviceControl firstDeviceControl, DeviceControl secondDeviceControl)
+        {
+            return new Dictionary<DeviceControl, RepeaterModel>
+            {
+                {
+                    secondDeviceControl,
+                    new RepeaterModel(firstDeviceControl, secondDeviceControl)
+                },
+            };
+        }
+
         #endregion
     }
 }
