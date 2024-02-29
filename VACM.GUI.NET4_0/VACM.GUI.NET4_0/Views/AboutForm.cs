8915c494d048c67a7723d2db68ffaec29bd173d5
Alex Portell <amportell.13@gmail.com>
2024-02-28 22:59:53 -0500

    Formatted lines by 80/24 rule. Removed getters.

--- VACM.GUI.NET4_0/VACM.GUI.NET4_0/Views/AboutForm.cs
+++ VACM.GUI.NET4_0/VACM.GUI.NET4_0/Views/AboutForm.cs
@@ -5,100 +5,40 @@ using System.Drawing;
 using System.Linq;
 using System.Reflection;
 using System.Windows.Forms;
+using VACM.GUI.NET4_0.ViewModels.Accessors;
 
 namespace VACM.GUI.NET4_0.Views
 {
     partial class AboutForm : Form
     {
+        #region Logic
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
         public AboutForm()
         {
             InitializeComponent();
-            this.Text = String.Format("About {0}", AssemblyTitle);
-            this.labelProductName.Text = AssemblyProduct;
-            this.labelVersion.Text = String.Format("Version {0}", AssemblyVersion);
-            this.labelCopyright.Text = AssemblyCopyright;
-            this.labelCompanyName.Text = AssemblyCompany;
-            this.textBoxDescription.Text = AssemblyDescription;
+
+            this.Text = String.Format
+                ("About {0}", AssemblyInformationAccessor.AssemblyTitle);
+
+            this.labelProductName.Text =
+                AssemblyInformationAccessor.AssemblyProduct;
+
+            this.labelVersion.Text = String.Format("Version {0}",
+                AssemblyInformationAccessor.AssemblyVersion);
+
+            this.labelCopyright.Text =
+                AssemblyInformationAccessor.AssemblyCopyright;
+
+            this.labelCompanyName.Text =
+                AssemblyInformationAccessor.AssemblyCompany;
+
+            this.textBoxDescription.Text =
+                AssemblyInformationAccessor.AssemblyDescription;
         }
 
-        #region Assembly Attribute Accessors
-
-        public string AssemblyTitle
-        {
-            get
-            {
-                object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyTitleAttribute), false);
-                if (attributes.Length > 0)
-                {
-                    AssemblyTitleAttribute titleAttribute = (AssemblyTitleAttribute)attributes[0];
-                    if (titleAttribute.Title != "")
-                    {
-                        return titleAttribute.Title;
-                    }
-                }
-                return System.IO.Path.GetFileNameWithoutExtension(Assembly.GetExecutingAssembly().CodeBase);
-            }
-        }
-
-        public string AssemblyVersion
-        {
-            get
-            {
-                return Assembly.GetExecutingAssembly().GetName().Version.ToString();
-            }
-        }
-
-        public string AssemblyDescription
-        {
-            get
-            {
-                object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyDescriptionAttribute), false);
-                if (attributes.Length == 0)
-                {
-                    return "";
-                }
-                return ((AssemblyDescriptionAttribute)attributes[0]).Description;
-            }
-        }
-
-        public string AssemblyProduct
-        {
-            get
-            {
-                object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyProductAttribute), false);
-                if (attributes.Length == 0)
-                {
-                    return "";
-                }
-                return ((AssemblyProductAttribute)attributes[0]).Product;
-            }
-        }
-
-        public string AssemblyCopyright
-        {
-            get
-            {
-                object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyCopyrightAttribute), false);
-                if (attributes.Length == 0)
-                {
-                    return "";
-                }
-                return ((AssemblyCopyrightAttribute)attributes[0]).Copyright;
-            }
-        }
-
-        public string AssemblyCompany
-        {
-            get
-            {
-                object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyCompanyAttribute), false);
-                if (attributes.Length == 0)
-                {
-                    return "";
-                }
-                return ((AssemblyCompanyAttribute)attributes[0]).Company;
-            }
-        }
         #endregion
     }
 }
