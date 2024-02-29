fe0df7527db1f95420a58dec87e3f48e59e5c470
Alex Portell <amportell.13@gmail.com>
2024-02-29 03:52:21 -0500

    Added readonly Color values and getters.

--- VACM/VACM.GUI.NET4_0/ViewModels/FormColorUpdater.cs
+++ VACM/VACM.GUI.NET4_0/ViewModels/FormColorUpdater.cs
@@ -1,11 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
 
 namespace VACM.GUI.NET4_0.ViewModels
 {
-    internal class FormColorUpdater
+    public class FormColorUpdater
     {
+        #region Parameters
+
+        private readonly static Color darkBackColor = Color.FromArgb(60, 63, 65);
+        private readonly static Color darkTextColor = Color.White;
+        private readonly static Color lightBackColor = Color.White;
+        private readonly static Color lightTextColor = Color.Black;
+
+        public static Color BackColor
+        {
+            get
+            {
+                if (!LightThemeValidator.IsLightThemeEnabled)
+                {
+                    return darkBackColor;
+                }
+                else
+                {
+                    return lightBackColor;
+                }
+            }
+        }
+
+        public static Color ForeColor
+        {
+            get
+            {
+                if (!LightThemeValidator.IsLightThemeEnabled)
+                {
+                    return darkTextColor;
+                }
+                else
+                {
+                    return lightTextColor;
+                }
+            }
+        }
+
+        #endregion
     }
 }
