60a9e03fb5bec9eff3871ed839c0bf90ce703b1d
Alex Portell <amportell.13@gmail.com>
2024-02-28 23:24:08 -0500

    Added WMI registry event listener class.

---
+++ VACM.GUI.NET4_0/VACM.GUI.NET4_0/Extensions/WMIRegistryEventListener.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace VACM.NET4.Extensions
+{
+    public class WMIRegistryEventListener
+    {
+        
+    }
+}
