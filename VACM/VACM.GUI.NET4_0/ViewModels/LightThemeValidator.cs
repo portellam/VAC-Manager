a6d35eb09d21b240efe88500dcbc7f96875e0fcf
Alex Portell <amportell.13@gmail.com>
2024-02-29 00:33:55 -0500

    Added bools to be used by program args and by forms to set color theme.

--- VACM/VACM.GUI.NET4_0/ViewModels/LightThemeValidator.cs
+++ VACM/VACM.GUI.NET4_0/ViewModels/LightThemeValidator.cs
@@ -10,6 +10,82 @@ namespace VACM.GUI.NET4_0.ViewModels
 {
     public class LightThemeValidator
     {
+        //TODO: add event watcher
+        //TODO: add on value change watcher here, which will watch for event watcher
+        //  value change. Ditto in main form and about form.
 
+        #region Parameters
+
+        private readonly static string appsUseLightThemeRegistryValueName =
+            "AppsUseLightTheme";
+
+        private readonly static string darkModeRegistryKeyPath =
+            @"Software\Microsoft\Windows\CurrentVersion\Themes\Personalize";
+
+        private readonly static string systemUsesLightThemeRegistryValueName =
+            "SystemUsesLightTheme";
+
+        private static bool AppsUseLightTheme
+        {
+            get
+            {
+                var result = Registry.CurrentUser?.OpenSubKey(darkModeRegistryKeyPath)
+                    .GetValue(appsUseLightThemeRegistryValueName);
+
+                return Convert.ToBoolean(result, CultureInfo.InvariantCulture);
+            }
+        }
+
+        private static bool SystemUsesLightTheme
+        {
+            get
+            {
+                var result = Registry.CurrentUser?.OpenSubKey(darkModeRegistryKeyPath)
+                    .GetValue(systemUsesLightThemeRegistryValueName);
+
+                return Convert.ToBoolean(result, CultureInfo.InvariantCulture);
+            }
+        }
+
+        private static bool isLightThemeEnabled = isLightThemeEnabledAtStart;
+
+        private readonly static bool isLightThemeEnabledAtStart =
+            IsLightThemeEnabledInRegistry;
+
+        public static bool DoesLightThemeDifferFromRegistry
+        {
+            get
+            {
+                return IsLightThemeEnabled != IsLightThemeEnabledInRegistry;
+            }
+        }
+
+        public static bool IsLightThemeEnabled
+        {
+            get
+            {
+                if (Program.DoesArgumentForceColorTheme)
+                {
+                    return Program.ForcedLightTheme;
+                }
+
+                return isLightThemeEnabled;
+            }
+            set
+            {
+                isLightThemeEnabled = value;
+            }
+        }
+
+        public static bool IsLightThemeEnabledInRegistry
+        {
+            get
+            {
+                return AppsUseLightTheme
+                    || SystemUsesLightTheme;
+            }
+        }
+
+        #endregion
     }
 }
