11558ef391fd00b5e371e8e5702ac354d18bef12
Alex Portell <amportell.13@gmail.com>
2024-02-29 03:38:38 -0500

    Added readonly values of dark colors for back and foreground.

--- VACM/VACM.GUI.NET4_0/ViewModels/ColorTable/DarkColorTable.cs
+++ VACM/VACM.GUI.NET4_0/ViewModels/ColorTable/DarkColorTable.cs
@@ -1,8 +1,11 @@
-﻿using System.Windows.Forms;
+﻿using System.Drawing;
+using System.Windows.Forms;
 
 namespace VACM.GUI.NET4_0.ViewModels.ColorTable
 {
     public class DarkColorTable : ProfessionalColorTable
     {
+        public readonly Color BackColor = Color.FromArgb(60, 63, 65);
+        public readonly Color ForeColor = Color.White;
     }
 }
