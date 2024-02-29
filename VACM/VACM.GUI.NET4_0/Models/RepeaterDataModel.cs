b44bfac04827c3d7a9497490461f46cb5dfef4e2
Alex Portell <amportell.13@gmail.com>
2024-02-29 03:05:17 -0500

    Added logic to validate existence of key value pair in dictionary.

--- VACM/VACM.GUI.NET4_0/Models/RepeaterDataModel.cs
+++ VACM/VACM.GUI.NET4_0/Models/RepeaterDataModel.cs
@@ -77,6 +77,80 @@ namespace VACM.GUI.NET4_0.Models
             };
         }
 
+        /// <summary>
+        /// Remove a repeater from the repeater data model,
+        /// if it does not already exist.
+        /// </summary>
+        /// <param name="firstDeviceControl">The first device control</param>
+        /// <param name="secondDeviceControl">The second device control</param>
+        public void RemoveDictionary
+            (DeviceControl firstDeviceControl, DeviceControl secondDeviceControl)
+        {
+            RemoveThisDictionary(firstDeviceControl, secondDeviceControl);
+            RemoveThisDictionary(secondDeviceControl, firstDeviceControl);
+        }
+
+        /// <summary>
+        /// Remove a dictionary from the repeater data model,
+        /// if it does not already exist.
+        /// </summary>
+        /// <param name="firstDeviceControl">The first device control</param>
+        /// <param name="secondDeviceControl">The second device control</param>
+        internal void RemoveThisDictionary
+            (DeviceControl firstDeviceControl, DeviceControl secondDeviceControl)
+        {
+            if (firstDeviceControl is null || secondDeviceControl is null)
+            {
+                return;
+            }
+
+            if (!DoesRepeaterDataValueContainKey
+                (firstDeviceControl, secondDeviceControl))
+            {
+                return;
+            }
+
+            RepeaterData[firstDeviceControl].Remove(secondDeviceControl);
+        }
+
+        /// <summary>
+        /// Does dictionary contain key.
+        /// </summary>
+        /// <param name="deviceControl">The device control</param>
+        /// <returns>True/False</returns>
+        internal bool DoesRepeaterDataContainKey(DeviceControl deviceControl)
+        {
+            if (deviceControl is null)
+            {
+                return false;
+            }
+
+            return RepeaterData.Keys.Select(x => x.MMDevice == deviceControl.MMDevice)
+                .FirstOrDefault();
+        }
+
+        /// <summary>
+        /// Does dictionary contain value.
+        /// </summary>
+        /// <param name="firstDeviceControl">The first device control</param>
+        /// <param name="secondDeviceControl">The second device control</param>
+        /// <returns>True/False</returns>
+        internal bool DoesRepeaterDataValueContainKey
+            (DeviceControl firstDeviceControl, DeviceControl secondDeviceControl)
+        {
+            if (secondDeviceControl is null
+                || DoesRepeaterDataContainKey(firstDeviceControl))
+            {
+                return false;
+            }
+
+            return RepeaterData.Values
+                .Select(thisFirstDeviceControl => thisFirstDeviceControl.Keys
+                    .Select(thisSecondDeviceControl =>
+                        thisSecondDeviceControl.MMDevice == firstDeviceControl.MMDevice)
+                    .FirstOrDefault()).FirstOrDefault();
+        }
+
         #endregion
     }
 }
