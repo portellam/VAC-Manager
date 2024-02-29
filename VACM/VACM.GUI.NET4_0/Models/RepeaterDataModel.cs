040a93876dc8f9dcc6b788545996cd7226dc6994
Alex Portell <amportell.13@gmail.com>
2024-02-29 03:01:49 -0500

    Began work on constructor.

--- VACM/VACM.GUI.NET4_0/Models/RepeaterDataModel.cs
+++ VACM/VACM.GUI.NET4_0/Models/RepeaterDataModel.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using VACM.GUI.NET4_0.ViewModels;
 
 namespace VACM.GUI.NET4_0.Models
 {
@@ -9,9 +10,22 @@ namespace VACM.GUI.NET4_0.Models
     {
         #region Parameters
 
+        public Dictionary<DeviceControl, Dictionary<DeviceControl, RepeaterModel>>
+            RepeaterData
+        { get; private set; }
+
         #endregion
 
         #region Logic
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public RepeaterDataModel()
+        {
+            RepeaterData = new Dictionary<DeviceControl,
+                Dictionary<DeviceControl, RepeaterModel>>();
+        }
 
         #endregion
     }
