feb2e7b505a9f7a117ee70ba4d2f3a019f183d1f
Alex Portell <amportell.13@gmail.com>
2024-03-15 04:35:50 -0400

    Added console project. Began work.

---
+++ VACM/VACM.Console.NET4_0/ConsoleWindow.cs
@@ -0,0 +1,18 @@
+﻿namespace VACM.Console.NET4_0
+{
+    public class ConsoleWindow
+    {
+        #region Logic
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public ConsoleWindow()
+        {
+            System.Console.WriteLine("Hello world!");
+            System.Console.Read();
+        }
+
+        #endregion
+    }
+}
---
+++ VACM/VACM.Console.NET4_0/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+
+namespace VACM.Console.NET4_0
+{
+    public class Program
+    {
+        #region Arguments
+
+        /// <summary>
+        /// The command line arguments.
+        /// </summary>
+        private static string[] Arguments;
+
+        #endregion
+
+        #region Logic
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        /// <param name="arguments">The command line arguments</param>
+        [STAThread]
+        internal static void Main(string[] arguments)
+        {
+            Arguments = arguments;
+            ParseArguments();
+            new ConsoleWindow();
+        }
+
+        /// <summary>
+        /// Parse arguments passed by command line.
+        /// </summary>
+        internal static void ParseArguments()
+        {
+            if (Arguments.Count() == 0)
+            {
+                return;
+            }
+
+            Arguments.ToList().ForEach(argument =>
+            {
+                if (!argument.StartsWith("/"))
+                {
+                    return;
+                }
+
+                switch (argument.ToLower())
+                {
+                    default:
+                        break;
+                }
+            });
+        }
+
+        #endregion
+    }
+}
