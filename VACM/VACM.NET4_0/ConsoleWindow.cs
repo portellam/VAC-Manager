4e10774309656db01129d4bbd4b2910b84a5ee3c
Alex Portell <amportell.13@gmail.com>
2024-03-15 03:07:27 -0400

    Began work on console logic.

---
+++ VACM/VACM.NET4_0/ConsoleWindow.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading;
+using VACM.NET4_0.Consoles;
+
+namespace VACM.NET4_0
+{
+    public class ConsoleWindow
+    {
+        #region Logic
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public ConsoleWindow()
+        {
+            var thread = new Thread(() =>
+            {
+                var mainConsole = new MainConsole();
+            });
+
+            thread.SetApartmentState(ApartmentState.STA);
+            thread.Start();
+        }
+
+        #endregion
+    }
+}
---
+++ VACM/VACM.NET4_0/Consoles/MainConsole.cs
@@ -0,0 +1,17 @@
+﻿namespace VACM.NET4_0.Consoles
+{
+    public class MainConsole
+    {
+        #region Logic
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public MainConsole()
+        {
+
+        }
+
+        #endregion
+    }
+}
