475c1ba4d6e28d8f4705848833b3b9e925e415e1
Alex Portell <amportell.13@gmail.com>
2024-03-15 03:38:56 -0400

    Added ConsoleControl. Added form for Console.

---
+++ VACM/VACM.NET4_0/Views/ConsoleForm.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace VACM.NET4_0.Views
+{
+    public partial class ConsoleForm : Form
+    {
+        public ConsoleForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
