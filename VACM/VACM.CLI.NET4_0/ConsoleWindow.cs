fa8245f6f522bf111b61129fe0104ac51f972799
Alex Portell <amportell.13@gmail.com>
2024-03-17 03:55:04 -0400

    Added logic to create and start a task, which is monitored by the Program.

--- VACM/VACM.CLI.NET4_0/ConsoleWindow.cs
+++ VACM/VACM.CLI.NET4_0/ConsoleWindow.cs
@@ -1,9 +1,28 @@
-﻿using VACM.CLI.Views.NET4_0;
+﻿using log4net;
+using System.Threading;
+using System.Threading.Tasks;
+using VACM.CLI.Views.NET4_0;
 
 namespace VACM.CLI.NET4_0
 {
     public class ConsoleWindow
     {
+        /*
+         * TODO:
+         * -add logic to parse Program argument flags and params, and influence the
+         *  constructor logic.
+         * 
+         */
+
+        #region Parameters
+
+        private static readonly ILog iLog = LogManager.GetLogger
+            (System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public Task Task { get; private set; }
+
+        #endregion
+
         #region Logic
 
         /// <summary>
@@ -11,7 +30,24 @@ namespace VACM.CLI.NET4_0
         /// </summary>
         public ConsoleWindow()
         {
-            new MainConsole();
+            iLog.Info($"Preparing {nameof(ConsoleWindow)}...");
+
+            PrepareTask();
+            iLog.Info($"Starting task...");
+            Task.Start();
+        }
+
+        /// <summary>
+        /// Prepare the task.
+        /// </summary>
+        internal void PrepareTask()
+        {
+            iLog.Info($"Preparing task...");
+
+            Task = new Task(() =>
+            {
+                new MainConsole();
+            });
         }
 
         #endregion
