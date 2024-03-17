353d9e8726ae18fe545465a086143f5ebe4f25c5
Alex Portell <amportell.13@gmail.com>
2024-03-17 03:55:26 -0400

    Created test logic for main console menu.

--- VACM/VACM.CLI.NET4_0/Views/MainConsole.cs
+++ VACM/VACM.CLI.NET4_0/Views/MainConsole.cs
@@ -1,12 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using log4net;
+using System;
+using VACM.GUI.NET4_0.ViewModels.Accessors;
 
 namespace VACM.CLI.Views.NET4_0
 {
     public class MainConsole
     {
+        #region Parameters
+
+        private string[] options = new string[]
+        {
+            "File",
+            "Devices",
+            "Links",
+            "Repeaters",
+            "About"
+        };
+
+        private static readonly ILog iLog = LogManager.GetLogger
+            (System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        #endregion
+
         #region Logic
 
         /// <summary>
@@ -14,8 +29,49 @@ namespace VACM.CLI.Views.NET4_0
         /// </summary>
         public MainConsole()
         {
-            Console.WriteLine("Hello world!");
+            iLog.Info($"Opening {nameof(MainConsole)}...");
+            FirstMenu();
+            PrintAndParseOptions();
             Console.Read();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        internal void FirstMenu()
+        {
+            Console.WriteLine(
+                $"Welcome to {AssemblyInformationAccessor.AssemblyProduct}!"
+            );
+        }
+
+        internal void PrintAndParseOptions()
+        {
+            iLog.Info($"Printing options...");
+
+            while (true)
+            {
+                for (int i = 0; i < options.Length; i++)
+                {
+                    string option = options[i];
+                    int index = i + 1;
+                    string line = string.Format("{0}. {1}", index, option);
+                    Console.WriteLine(line);
+                }
+
+                Console.Write($"Enter [{1}-{options.Length}]:\t");
+                string input = Console.ReadLine();
+
+                if (string.IsNullOrEmpty(input) || !int.TryParse(input, out int result)
+                    || result < 1 || result > options.Length)
+                {
+                    Console.WriteLine("Invalid option. Please try again.");
+                    continue;
+                }
+
+                result--;
+                break;
+            }
         }
 
         #endregion
