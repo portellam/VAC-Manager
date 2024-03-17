184bdf34f0108f2f1abacaffaae1466a92a6a18c
Alex Portell <amportell.13@gmail.com>
2024-03-17 04:27:04 -0400

    Began work on new device model for proposed "nested" dicts, to help with avoiding creating nasty lambda logic.

---
+++ VACM/VACM.GUI.NET4_0/Models/DeviceModel.cs
@@ -0,0 +1,52 @@
+﻿using NAudio.CoreAudioApi;
+
+namespace VACM.GUI.NET4_0.Models
+{
+    public class DeviceModel
+    {
+        #region Parameters
+
+        public bool IsChecked { get; set; } = false;
+        public bool IsSelected { get; set; } = false;
+        public MMDevice MMDevice { get; private set; }
+
+        #endregion
+
+        #region Logic
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="mMDevice">The MMDevice</param>
+        public DeviceModel(MMDevice mMDevice)
+        {
+            MMDevice = mMDevice;
+        }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="mMDevice">The MMDevice</param>
+        /// <param name="isSelected">Is selected</param>
+        public DeviceModel(MMDevice mMDevice, bool isSelected)
+        {
+            MMDevice = mMDevice;
+            IsChecked = isSelected;
+        }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="mMDevice">The MMDevice</param>
+        /// <param name="isSelected">Is selected</param>
+        /// <param name="isChecked">Is checked</param>
+        public DeviceModel(MMDevice mMDevice, bool isSelected, bool isChecked)
+        {
+            MMDevice = mMDevice;
+            IsChecked = isSelected;
+            IsChecked = isChecked;
+        }
+
+        #endregion
+    }
+}
