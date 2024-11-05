836cb18c7afa79a579cb9a6d75aa8f25e31011d3
user <mail@example.com>
2024-11-04 18:09:43 -0800

    Added Device Model.

---
+++ VACM/VACM.NET4_0/Models/Device.cs
@@ -0,0 +1,83 @@
+﻿using NAudio.CoreAudioApi;
+
+namespace VACM.NET4_0.Models
+{
+  public class Device
+  {
+    #region Parameters
+    public string Id { get; private set; }
+    public bool IsInput { get; private set; }
+    public bool IsOutput { get; private set; }
+    public bool IsPresent { get; private set; }
+    public bool IsSelected { get; set; }
+    public string Name { get; private set; }
+
+    private DeviceState Present =
+      DeviceState.Active
+      | DeviceState.Unplugged
+      | DeviceState.Disabled;
+
+    #endregion
+
+    #region Constructors
+    public Device
+      (
+        MMDevice mMDevice,
+        bool isSelected
+      )
+    {
+      Id = mMDevice.ID;
+      Name = mMDevice.FriendlyName;
+      IsPresent = mMDevice.State == Present;
+      IsSelected = isSelected;
+      IsInput = false;
+      IsOutput = false;
+
+      SetDataFlow(mMDevice.DataFlow);
+    }
+
+    public Device
+      (
+        string id,
+        string name,
+        DeviceState deviceState,
+        DataFlow dataFlow,
+        bool isSelected
+      )
+    {
+      Id = id;
+      Name = name;
+      IsPresent = deviceState == Present;
+      IsSelected = isSelected;
+      IsInput = false;
+      IsOutput = false;
+
+      SetDataFlow(dataFlow);
+    }
+    #endregion
+
+    public void ToggleIsPresent()
+    {
+      IsPresent = !IsPresent;
+    }
+
+    public void ToggleIsSelected()
+    {
+      IsSelected = !IsSelected;
+    }
+
+    private void SetDataFlow(DataFlow dataFlow)
+    {
+      IsInput = DataFlow.Capture == dataFlow;
+      IsOutput = DataFlow.Render == dataFlow;
+
+      if (dataFlow != DataFlow.All)
+      {
+        return;      
+      }
+
+      IsInput = true;
+      IsInput = true;
+    }
+  }
+}
