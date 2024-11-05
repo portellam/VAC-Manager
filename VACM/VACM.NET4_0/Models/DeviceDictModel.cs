80729d8441566059c8187c9149e2147cfc7c1231
user <mail@example.com>
2024-11-04 18:10:18 -0800

    Added Device Dict Model.

---
+++ VACM/VACM.NET4_0/Models/DeviceDictModel.cs
@@ -0,0 +1,115 @@
+﻿using NAudio.CoreAudioApi;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace VACM.NET4_0.Models
+{
+  public class DeviceDictModel
+  {
+    #region Parameters
+    private List<MMDevice> MMDeviceList { get; set; }
+
+    public List<Device> DeviceList { get; private set; }
+
+    public Dictionary<string, string> PresentWaveInDeviceList
+    {
+      get
+      {
+        return DeviceList
+          .Where(x => x.IsInput && x.IsPresent)
+          .ToDictionary(x => x.Id, x => x.Name);
+      }
+    }
+
+    public Dictionary<string, string> PresentWaveOutDeviceList
+    {
+      get
+      {
+        return DeviceList
+          .Where(x => x.IsOutput && x.IsPresent)
+          .ToDictionary(x => x.Id, x => x.Name);
+      }
+    }
+
+    public Dictionary<string, string> SelectedWaveInDeviceList
+    {
+      get
+      {
+        return DeviceList
+          .Where(x => x.IsInput && x.IsSelected)
+          .ToDictionary(x => x.Id, x => x.Name);
+      }
+    }
+
+    public Dictionary<string, string> SelectedWaveOutDeviceList
+    {
+      get
+      {
+        return DeviceList
+          .Where(x => x.IsOutput && x.IsSelected)
+          .ToDictionary(x => x.Id, x => x.Name);
+      }
+    }
+
+    public Dictionary<string, string> WaveInDeviceList
+    {
+      get
+      {
+        return DeviceList
+          .Where(x => x.IsInput)
+          .ToDictionary(x => x.Id, x => x.Name);
+      }
+    }
+
+    public Dictionary<string, string> WaveOutDeviceList
+    {
+      get
+      {
+        return DeviceList
+          .Where(x => x.IsOutput)
+          .ToDictionary(x => x.Id, x => x.Name);
+      }
+    }
+    #endregion
+
+    #region Constructors
+    public DeviceDictModel()
+    {
+      SetMMDeviceList();
+
+      if(MMDeviceList.Count == 0)
+      {
+        return;
+      }
+
+      bool isSelected = false;
+      DeviceList = new List<Device>();
+
+      MMDeviceList
+        .ForEach(x => DeviceList
+          .Add
+          (
+            new Device
+            (
+              x,
+              isSelected
+            )
+          )
+        );
+    }
+    #endregion
+    
+    public void SetMMDeviceList()
+    {
+      MMDeviceList = new MMDeviceEnumerator()
+        .EnumerateAudioEndPoints
+        (
+          DataFlow.All,
+          DeviceState.All
+        )
+        .Distinct()
+        .OrderBy(x => x.ID)
+        .ToList();
+    }
+  }
+}
