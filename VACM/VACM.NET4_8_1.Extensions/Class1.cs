1e3e2884025bf5cfbcb8eda85657e8c290417b6a
portellam <amportell.13@gmail.com>
2024-11-06 17:39:26 -0500

    Added NET 4.8.1 projects.

---
+++ VACM/VACM.NET4_8_1.Extensions/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VACM.NET4_8_1.Extensions
+{
+    public class Class1
+    {
+    }
+}
