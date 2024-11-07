4c5ed3a9a0d05aa8ff90d637d0575b600bc82209
portellam <amportell.13@gmail.com>
2024-11-06 20:26:15 -0500

    Added extension classes to dedicated project.

---
+++ VACM/VACM.NET4_0.Extensions/PropertyValueChanged/PropertyValueChangedDelegate.cs
@@ -0,0 +1,8 @@
+﻿namespace VACM.NET4_0.Extensions.PropertyValueChanged
+{
+  public delegate void PropertyValueChangedDelegate
+  (
+    object sender,
+    PropertyValueChangedEventArgs valueUpdatedEventArgs
+  );
+}
---
+++ VACM/VACM.NET4_0.Extensions/RegistryKeyChanged/RegistryKeyChangedDelegate.cs
@@ -0,0 +1,8 @@
+﻿namespace VACM.NET4_0.Extensions.RegistrySubKeyChanged
+{
+  public delegate void RegistrySubKeyChangedDelegate
+  (
+    object sender,
+    RegistrySubKeyChangedEventArgs registrySubKeyChangedEventArgs
+  );
+}
