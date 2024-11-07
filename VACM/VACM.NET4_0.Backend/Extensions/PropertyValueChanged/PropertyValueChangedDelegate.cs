13733b6fc62876d35afb338b370504ed7900dc1d
portellam <amportell.13@gmail.com>
2024-11-06 20:27:34 -0500

    Removed extensions.

--- VACM/VACM.NET4_0.Backend/Extensions/PropertyValueChanged/PropertyValueChangedDelegate.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace VACM.NET4_0.Backend.Extensions.PropertyValueChanged
-{
-  public delegate void PropertyValueChangedDelegate
-  (
-    object sender,
-    PropertyValueChangedEventArgs valueUpdatedEventArgs
-  );
-}
--- VACM/VACM.NET4_0.Backend/Extensions/RegistryKeyChanged/RegistryKeyChangedDelegate.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace VACM.NET4_0.Backend.Extensions.RegistrySubKeyChanged
-{
-  public delegate void RegistrySubKeyChangedDelegate
-  (
-    object sender,
-    RegistrySubKeyChangedEventArgs registrySubKeyChangedEventArgs
-  );
-}
