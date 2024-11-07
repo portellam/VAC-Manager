f88e144a3bf97c09c490692418131cb87a50b235
portellam <amportell.13@gmail.com>
2024-11-07 16:41:43 -0500

    Updated assemblies for NET 4.8 (named as NET 4.8.1) projects.

--- VACM/VACM.NET4_8_1.GUI/Properties/AssemblyInfo.cs
+++ VACM/VACM.NET4_8_1.GUI/Properties/AssemblyInfo.cs
@@ -5,13 +5,13 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("VACM.NET4_8_1.GUI")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("VAC Manager")]
+[assembly: AssemblyDescription("Graphical user interface (GUI) to create, manage, and automate instances of Virtual Audio Cable (VAC) audio repeaters. This application version is targeted for either 32-bit (x86) or 64-bit (x64) versions of Virtual Audio Cable. Required dependencies include Microsoft .NET Framework version 4.8. Supported operating systems include Microsoft Windows 7 SP1/Server 2008 R2 or newer.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("VACM.NET4_8_1.GUI")]
-[assembly: AssemblyCopyright("Copyright ©  2024")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyProduct("Virtual Audio Cable Manager")]
+[assembly: AssemblyCopyright("VAC Manager Copyleft © 2024 Alexander Portell.\nVirtual Audio Cable Copyright © 1998-2024 Eugene V. Muzychenko.")]
+[assembly: AssemblyTrademark("VACM")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
