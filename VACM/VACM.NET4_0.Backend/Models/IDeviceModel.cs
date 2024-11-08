7b09a143e68e67a0bee8f285eca60685d0de0ca1
portellam <amportell.13@gmail.com>
2024-11-08 14:58:49 -0500

    Created interface for DeviceModel.

--- VACM/VACM.NET4_0.Backend/Models/IDeviceModel.cs
+++ VACM/VACM.NET4_0.Backend/Models/IDeviceModel.cs
@@ -1,11 +1,20 @@
-﻿namespace VACM.NET4_0.Backend.Models
+﻿using System.ComponentModel;
+
+namespace VACM.NET4_0.Backend.Models
 {
   public interface IDeviceModel
   {
-    string Id { get; set; }
-    string Name { get; set; }
+    #region Parameters
+
+    int Id { get; set; }
+    bool IsDuplex { get; set; }
     bool IsInput { get; set; }
     bool IsOutput { get; set; }
     bool IsPresent { get; set; }
+    event PropertyChangedEventHandler PropertyChanged;
+    string ActualId { get; set; }
+    string Name { get; set; }
+
+    #endregion
   }
 }
