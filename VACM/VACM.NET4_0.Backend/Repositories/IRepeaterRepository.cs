a33b2f403e88b1906890f97527587aaf9bf9fed5
portellam <amportell.13@gmail.com>
2024-11-08 20:38:24 -0500

    Update IRepeaterRepository.cs

--- VACM/VACM.NET4_0.Backend/Repositories/IRepeaterRepository.cs
+++ VACM/VACM.NET4_0.Backend/Repositories/IRepeaterRepository.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 
@@ -7,5 +8,14 @@ namespace VACM.NET4_0.Backend.Repositories
 {
   public interface IRepeaterRepository
   {
+    #region Parameters
+
+    event PropertyChangedEventHandler PropertyChanged;
+
+    #endregion
+
+    #region Logic
+
+    #endregion
   }
 }
