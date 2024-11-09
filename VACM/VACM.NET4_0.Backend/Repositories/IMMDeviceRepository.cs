698cf36da0597170e022e3bbc1d477340e4bf1f1
portellam <amportell.13@gmail.com>
2024-11-08 20:38:21 -0500

    Update IMMDeviceRepository.cs

--- VACM/VACM.NET4_0.Backend/Repositories/IMMDeviceRepository.cs
+++ VACM/VACM.NET4_0.Backend/Repositories/IMMDeviceRepository.cs
@@ -1,11 +1,25 @@
-﻿using System;
+﻿using NAudio.CoreAudioApi;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.ComponentModel;
 
 namespace VACM.NET4_0.Backend.Repositories
 {
   public interface IMMDeviceRepository
   {
+    #region Parameters
+
+    event PropertyChangedEventHandler PropertyChanged;
+
+    #endregion
+
+    #region Logic
+
+    List<MMDevice> GetRange(List<string> idList);
+    MMDevice Get(string id);
+    void Disable(string id);
+    void Enable(string id);
+    void SetList();
+
+    #endregion
   }
 }
