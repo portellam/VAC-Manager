ac52b16fd11b516a682e45af1de6f60170b806d2
portellam <amportell.13@gmail.com>
2024-11-11 02:50:28 -0500

    Update IDeviceController.cs

--- VACM/VACM.NET4_0.Backend/Controllers/IDeviceController.cs
+++ VACM/VACM.NET4_0.Backend/Controllers/IDeviceController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NAudio.CoreAudioApi;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,5 +8,25 @@ namespace VACM.NET4_0.Backend.Controllers
 {
   public interface IDeviceController
   {
+
+    void Insert(MMDevice mMDevice);
+
+    void Insert
+    (
+      string actualId,
+      string name,
+      bool? isInput,
+      bool? isOutput,
+      bool? isPresent
+    );
+    void Remove(int? id);
+
+    void Remove(string actualId);
+
+    void Update
+    (
+      int? id,
+      MMDevice mMDevice
+    );
   }
 }
