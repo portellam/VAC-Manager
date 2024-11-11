d8cac47fc1057ef9390658c81825fde186e97157
portellam <amportell.13@gmail.com>
2024-11-11 02:37:13 -0500

    Added interfaces.

---
+++ VACM/VACM.NET4_0.Backend/Controllers/IRepeaterController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VACM.NET4_0.Backend.Controllers
+{
+  public interface IRepeaterController
+  {
+  }
+}
