d83e221a89b6b161d721f333a464350aab3ff0f7
portellam <amportell.13@gmail.com>
2024-11-12 02:56:52 -0500

    Create Global.cs

---
+++ VACM/VACM.NET4_0.Backend/Global.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace VACM.NET4_0.Backend
+{
+  /*
+   * TODO:
+   * - add flags to toggle behavior.
+   */
+
+  public class Global
+  {
+    #region Parameters
+
+    /// <summary>
+    /// Limit the number of endpoints (audio devices).
+    /// Note: Windows NT 6.x and newer does not have a maximum number of endpoints.
+    /// </summary>
+    public static uint MaxEndpointCount
+    {
+      get
+      {
+        if (Environment.OSVersion.Version.Major < 6)
+        {
+          return WindowsNT5MaxEndpointCount;
+        }
+
+        return VACMaxVirtualEndpointCount;
+      }
+    }
+
+    /// <summary>
+    /// Maximum amount of virtual endpoints (virtual audio cables) for VAC.
+    /// </summary>
+    public static uint VACMaxVirtualEndpointCount
+    {
+      get
+      {
+        return 256;
+      }
+    }
+
+    /// <summary>
+    /// Maximum amount of endpoints (audio devices) for Windows 5.x and older.
+    /// </summary>
+    public static uint WindowsNT5MaxEndpointCount
+    {
+      get
+      {
+        return 32;
+      }
+    }
+
+
+    #endregion
+  }
+}
