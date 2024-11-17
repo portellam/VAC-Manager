7858d3017157d012ae46ec7cc4c80c02bb5b5f30
portellam <amportell.13@gmail.com>
2024-11-17 01:54:39 -0500

    Setup repeater interface.

--- VACM/VACM.NET4_0.Backend/Controllers/IRepeaterController.cs
+++ VACM/VACM.NET4_0.Backend/Controllers/IRepeaterController.cs
@@ -1,11 +1,68 @@
-﻿using System;
+﻿using NAudio.CoreAudioApi;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.ComponentModel;
+using VACM.NET4_0.Backend.Models;
+using VACM.NET4_0.Backend.Structs;
 
 namespace VACM.NET4_0.Backend.Controllers
 {
   public interface IRepeaterController
   {
+    #region Parameters
+
+    event PropertyChangedEventHandler PropertyChanged;
+
+    #endregion
+
+    #region Logic
+
+    DeviceModel Get(string actualId);
+    DeviceModel Get(uint? id);
+    List<DeviceModel> GetAllAbsent();
+    List<DeviceModel> GetAll();
+    List<DeviceModel> GetAllInput();
+    List<DeviceModel> GetAllOutput();
+    List<DeviceModel> GetAllPresent();
+    void Insert(MMDevice mMDevice);
+
+    void Insert
+    (
+      string actualId,
+      string name,
+      bool? isInput,
+      bool? isOutput,
+      bool? isPresent
+    );
+
+    void Remove
+    (
+      uint? id
+    );
+
+    void Update
+    (
+      uint? id,
+      uint? inputDeviceId,
+      uint? outputDeviceId
+    );
+
+    void Update
+    (
+      uint? id,
+      uint? inputDeviceId,
+      uint? outputDeviceId,
+      byte bitsPerSample,
+      byte bufferAmount,
+      byte channelCount,
+      byte prefillPercentage,
+      byte resyncAtPercentage,
+      List<Channel> channelList,
+      string pathName,
+      string windowName,
+      uint sampleRateKHz,
+      ushort bufferDurationMs
+    );
+
+    #endregion
   }
 }
