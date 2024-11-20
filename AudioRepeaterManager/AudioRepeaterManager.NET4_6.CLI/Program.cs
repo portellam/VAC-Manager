d6ecdd753589cf58638989b858d78d684693336a
portellam <amportell.13@gmail.com>
2024-11-19 21:46:21 -0500

    Renamed namespaces.

--- AudioRepeaterManager/AudioRepeaterManager.NET4_6.CLI/Program.cs
+++ AudioRepeaterManager/AudioRepeaterManager.NET4_6.CLI/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace AudioRepeaterManager.NET4_6.CLI
+namespace AudioRepeaterManager.NET4_6.TUI
 {
   internal class Program
   {
