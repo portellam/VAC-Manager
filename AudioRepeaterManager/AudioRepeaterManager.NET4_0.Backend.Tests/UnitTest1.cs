e39ba00a11020c72b818dc26456bf08c6690d8b0
portellam <amportell.13@gmail.com>
2024-11-26 03:10:18 -0500

    Added test project for backend net4.0

---
+++ AudioRepeaterManager/AudioRepeaterManager.NET4_0.Backend.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace AudioRepeaterManager.NET4_0.Backend.Tests
+{
+  [TestClass]
+  public class UnitTest1
+  {
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+  }
+}
