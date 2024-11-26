f0b9159b71382c10d17bf8d15f542fb2ec23223a
portellam <amportell.13@gmail.com>
2024-11-26 11:11:02 -0500

    Cloned NET4.0 backend project to 4.6. Updated namespaces. Removed some comments.

---
+++ AudioRepeaterManager/AudioRepeaterManager.NET4_6.Backend/Structs/Channel.cs
@@ -0,0 +1,20 @@
+﻿namespace AudioRepeaterManager.NET4_6.Backend.Structs
+{
+  /// <summary>
+  /// The masks of individual speakers/channels.
+  /// </summary>
+  public enum Channel
+  {
+    FL = 0x1,
+    FR = 0x2,
+    FC = 0x4,
+    LF = 0x8,
+    BL = 0x10,
+    BR = 0x20,
+    FLC = 0x40,
+    FRC = 0x80,
+    BC = 0x100,
+    SL = 0x200,
+    SR = 0x400
+  }
+}
