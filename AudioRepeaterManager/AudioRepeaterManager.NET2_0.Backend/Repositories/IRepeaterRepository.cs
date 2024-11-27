d7cd3ca36f4c2fd3bb260b38c456d4447ee05386
portellam <amportell.13@gmail.com>
2024-11-27 02:05:11 -0500

    Cloned NET 4.8 backend project to 8.0. Updated namespaces. Removed AudioSwitcher library and code.

---
+++ AudioRepeaterManager/AudioRepeaterManager.NET2_0.Backend/Repositories/IRepeaterRepository.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using AudioRepeaterManager.NET2_0.Backend.Models;
+
+namespace AudioRepeaterManager.NET2_0.Backend.Repositories
+{
+  public interface IRepeaterRepository
+  {
+    #region Parameters
+
+    event PropertyChangedEventHandler PropertyChanged;
+
+    #endregion
+
+    #region Logic
+
+    RepeaterModel Get(uint? id);
+
+    RepeaterModel Get
+    (
+      uint? firstDeviceId,
+      uint? secondDeviceId
+    );
+
+    List<RepeaterModel> GetAll();
+
+    List<RepeaterModel> GetRange
+    (
+      string deviceName,
+      bool isInputDevice,
+      bool isOutputDevice
+    );
+
+    List<RepeaterModel> GetRange(List<uint?> idList);
+    void Insert(RepeaterModel repeaterModel);
+
+    void Insert
+    (
+      uint id,
+      uint inputDeviceId,
+      uint outputDeviceId,
+      byte bitsPerSample,
+      byte bufferAmount,
+      byte prefillPercentage,
+      byte resyncAtPercentage,
+      string inputDeviceName,
+      string outputDeviceName,
+      string pathName,
+      string windowName,
+      uint channelMask,
+      uint sampleRateKHz,
+      ushort bufferDurationMs
+    );
+
+    void Remove
+    (
+      uint? id
+    );
+
+    void Remove
+    (
+      uint? firstDeviceId,
+      uint? secondDeviceId
+    );
+
+    void RemoveRange
+    (
+      string deviceName
+    );
+
+    void Update(RepeaterModel repeaterModel);
+
+    void Update
+    (
+      uint id,
+      uint inputDeviceId,
+      uint outputDeviceId,
+      byte bitsPerSample,
+      byte bufferAmount,
+      byte prefillPercentage,
+      byte resyncAtPercentage,
+      string inputDeviceName,
+      string outputDeviceName,
+      string pathName,
+      string windowName,
+      uint channelMask,
+      uint sampleRateKHz,
+      ushort bufferDurationMs
+    );
+
+    #endregion
+  }
+}
---
+++ AudioRepeaterManager/AudioRepeaterManager.NET2_0.Backend/Structs/Direction.cs
@@ -0,0 +1,8 @@
+﻿namespace AudioRepeaterManager.NET2_0.Backend.Structs
+{
+    public enum Direction
+    {
+        Input = 0,
+        Output = 1
+    }
+}
