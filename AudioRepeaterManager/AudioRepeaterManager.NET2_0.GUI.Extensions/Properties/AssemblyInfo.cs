c0ab19eb03425d1d8cb061429563089fa4a9c942
portellam <amportell.13@gmail.com>
2024-11-30 00:34:34 -0500

    Distinguished assemblies for extension projects.

--- AudioRepeaterManager/AudioRepeaterManager.NET2_0.GUI.Extensions/Properties/AssemblyInfo.cs
+++ AudioRepeaterManager/AudioRepeaterManager.NET2_0.GUI.Extensions/Properties/AssemblyInfo.cs
@@ -4,8 +4,8 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("VAC Audio Repeater Manager Backend Extensions")]
-[assembly: AssemblyDescription("Backend Extensions for VAC Audio Repeater Manager.")]
+[assembly: AssemblyTitle("VAC Audio Repeater Manager GUI Extensions")]
+[assembly: AssemblyDescription("GUI Extensions for VAC Audio Repeater Manager.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Audio Repeater Manager for Virtual Audio Cable")]
@@ -18,7 +18,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("a85a10f1-4e75-4e79-be66-07eb62177f37")]
+[assembly: Guid("37f78a22-c819-4701-acb7-1a2968f763d3")]
 
 // Version information for an assembly consists of the following four values:
 //
