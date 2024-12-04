d7d25996b504a1bbe91d1a5b1efb3c01eceff858
portellam <amportell.13@gmail.com>
2024-12-04 01:41:49 -0500

    Added classes.

---
+++ AudioRepeaterManager.NET2/ARM.NET2_0.Backend/Models/IDeviceModel.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel;
+
+namespace AudioRepeaterManager.NET2_0.Backend.Models
+{
+  public interface IDeviceModel
+  {
+    #region Parameters
+
+    /// <summary>
+    /// Primary Key
+    /// </summary>
+    uint Id { get; set; }
+
+    /// <summary>
+    /// Foreign key
+    /// </summary>
+    string ActualId { get; set; }
+
+    bool IsDuplex { get; }
+    bool IsInput { get; set; }
+    bool IsOutput { get; set; }
+    bool IsPresent { get; set; }
+    event PropertyChangedEventHandler PropertyChanged;
+    string Name { get; set; }
+
+    #endregion
+
+    #region Logic
+
+    void Deconstruct
+    (
+      out uint id,
+      out string actualId,
+      out string name,
+      out bool? isInput,
+      out bool? isOutput,
+      out bool? isPresent
+    );
+
+    #endregion
+  }
+}
--- AudioRepeaterManager.NET2/ARM.NET2_0.Backend/Properties/AssemblyInfo.cs
+++ AudioRepeaterManager.NET2/ARM.NET2_0.Backend/Properties/AssemblyInfo.cs
@@ -2,35 +2,30 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ARM.NET2_0.Backend")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("VAC Audio Repeater Manager Backend")]
+[assembly: AssemblyDescription("Backend for VAC Audio Repeater Manager.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ARM.NET2_0.Backend")]
-[assembly: AssemblyCopyright("Copyright ©  2024")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyProduct("Audio Repeater Manager for Virtual Audio Cable")]
+[assembly: AssemblyCopyright("VAC Audio Repeater Manager Copyleft © 2024 Alexander Portell.\nVirtual Audio Cable Copyright © 1998-2024 Eugene V. Muzychenko.")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("c64eed1c-bab0-4ae9-a2ff-7ae7773f0ca6")]
+[assembly: Guid("aa6e6dc2-b99f-4a73-9831-bc21686a44f8")]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version 
+//      Minor Version
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
