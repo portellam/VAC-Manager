bfaa5c88cd707dbeae6ba2818e949d3d51ab61fc
portellam <amportell.13@gmail.com>
2024-12-13 12:28:13 -0500

    Update MainForm.cs

--- AudioRepeaterManager.NET8/AudioRepeaterManager.NET8.GUI/MainForm.cs
+++ AudioRepeaterManager.NET8/AudioRepeaterManager.NET8.GUI/MainForm.cs
@@ -57,178 +57,6 @@ namespace AudioRepeaterManager.NET8.GUI
     #endregion
 
     #region Repeaters logic
-    #endregion
-
-    #region View logic
-
-    private void alwaysOnTopToolStripMenuItem_Click
-    (
-      object sender,
-      EventArgs e
-    )
-    {
-
-    }
-
-    private void preferSystemThemeToolStripMenuItem_Click
-    (
-      object sender,
-      EventArgs e
-    )
-    {
-
-    }
-
-    private void preferDarkThemeToolStripMenuItem_Click
-    (
-      object sender,
-      EventArgs e
-    )
-    {
-
-    }
-
-    private void toggleFullScreenModeToolStripMenuItem_Click
-    (
-      object sender,
-      EventArgs e
-    )
-    {
-
-    }
-
-    #endregion
-
-    #region Settings logic
-
-    private void preferX64Application64bitToolStripMenuItem_Click
-    (
-      object sender,
-      EventArgs e
-    )
-    {
-
-    }
-
-    private void preferX86Application32bitToolStripMenuItem_Click
-    (
-      object sender,
-      EventArgs e
-    )
-    {
-
-    }
-
-    private void setApplicationPathToolStripMenuItem_Click
-    (
-      object sender,
-      EventArgs e
-    )
-    {
-
-    }
-
-    private void startAllRepeatersOnLoadToolStripMenuItem_Click
-    (
-      object sender,
-      EventArgs e
-    )
-    {
-
-    }
-
-    private void toggleBogusModeToolStripMenuItem_Click
-    (
-      object sender,
-      EventArgs e
-    )
-    {
-
-    }
-
-    private void toggleSafeModeToolStripMenuItem_Click
-    (
-      object sender,
-      EventArgs e
-    )
-    {
-
-    }
-
-    #endregion
-
-    #region Help logic
-
-    private void aboutToolStripMenuItem_Click
-    (
-      object sender,
-      EventArgs e
-    )
-    {
-      new AboutForm()
-        .ShowDialog();
-    }
-    private void commandLineArgumentsToolStripMenuItem_Click
-    (
-      object sender,
-      EventArgs e
-    )
-    {
-
-    }
-
-    private void vacWebsiteToolStripMenuItem_Click
-    (
-      object sender,
-      EventArgs e
-    )
-    {
-      string url = "https://vac.muzychenko.net";
-      UrlRedirectHelper.GoToSite(url);
-    }
-
-    private void websiteToolStripMenuItem_Click
-    (
-      object sender,
-      EventArgs e
-    )
-    {
-      string url = "https://www.github.com/portellam/vac-audio-repeater-manager";
-      UrlRedirectHelper.GoToSite(url);
-    }
-
-    #endregion
-
-    #region Window logic
-
-    private void sortByToolStripMenuItem_Click
-    (
-      object sender,
-      EventArgs e
-    )
-    {
-
-    }
-
-    private void windowsToolStripMenuItem_Click
-    (
-      object sender,
-      EventArgs e
-    )
-    {
-
-    }
-
-    private void windowToolStripMenuItem_Click
-    (
-      object sender,
-      EventArgs e
-    )
-    {
-
-    }
-
-    #endregion
 
     private void repeaterExportToClipboardToolStripMenuItem_Click
     (
@@ -409,5 +237,178 @@ namespace AudioRepeaterManager.NET8.GUI
     {
 
     }
+
+    #endregion
+
+    #region View logic
+
+    private void alwaysOnTopToolStripMenuItem_Click
+    (
+      object sender,
+      EventArgs e
+    )
+    {
+
+    }
+
+    private void preferSystemThemeToolStripMenuItem_Click
+    (
+      object sender,
+      EventArgs e
+    )
+    {
+
+    }
+
+    private void preferDarkThemeToolStripMenuItem_Click
+    (
+      object sender,
+      EventArgs e
+    )
+    {
+
+    }
+
+    private void toggleFullScreenModeToolStripMenuItem_Click
+    (
+      object sender,
+      EventArgs e
+    )
+    {
+
+    }
+
+    #endregion
+
+    #region Settings logic
+
+    private void preferX64Application64bitToolStripMenuItem_Click
+    (
+      object sender,
+      EventArgs e
+    )
+    {
+
+    }
+
+    private void preferX86Application32bitToolStripMenuItem_Click
+    (
+      object sender,
+      EventArgs e
+    )
+    {
+
+    }
+
+    private void setApplicationPathToolStripMenuItem_Click
+    (
+      object sender,
+      EventArgs e
+    )
+    {
+
+    }
+
+    private void startAllRepeatersOnLoadToolStripMenuItem_Click
+    (
+      object sender,
+      EventArgs e
+    )
+    {
+
+    }
+
+    private void toggleBogusModeToolStripMenuItem_Click
+    (
+      object sender,
+      EventArgs e
+    )
+    {
+
+    }
+
+    private void toggleSafeModeToolStripMenuItem_Click
+    (
+      object sender,
+      EventArgs e
+    )
+    {
+
+    }
+
+    #endregion
+
+    #region Help logic
+
+    private void aboutToolStripMenuItem_Click
+    (
+      object sender,
+      EventArgs e
+    )
+    {
+      new AboutForm()
+        .ShowDialog();
+    }
+    private void commandLineArgumentsToolStripMenuItem_Click
+    (
+      object sender,
+      EventArgs e
+    )
+    {
+
+    }
+
+    private void vacWebsiteToolStripMenuItem_Click
+    (
+      object sender,
+      EventArgs e
+    )
+    {
+      string url = "https://vac.muzychenko.net";
+      UrlRedirectHelper.GoToSite(url);
+    }
+
+    private void websiteToolStripMenuItem_Click
+    (
+      object sender,
+      EventArgs e
+    )
+    {
+      string url = "https://www.github.com/portellam/vac-audio-repeater-manager";
+      UrlRedirectHelper.GoToSite(url);
+    }
+
+    #endregion
+
+    #region Window logic
+
+    private void sortByToolStripMenuItem_Click
+    (
+      object sender,
+      EventArgs e
+    )
+    {
+
+    }
+
+    private void windowsToolStripMenuItem_Click
+    (
+      object sender,
+      EventArgs e
+    )
+    {
+
+    }
+
+    private void windowToolStripMenuItem_Click
+    (
+      object sender,
+      EventArgs e
+    )
+    {
+
+    }
+
+    #endregion
   }
 }
