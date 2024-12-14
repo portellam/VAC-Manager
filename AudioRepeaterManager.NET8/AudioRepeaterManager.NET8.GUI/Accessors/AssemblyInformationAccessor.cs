d7b2d774dacf7996424417697682bdd394b37481
portellam <amportell.13@gmail.com>
2024-12-14 02:36:53 -0500

    Updated assemblies and globals.

---
+++ AudioRepeaterManager.NET8/AudioRepeaterManager.NET8.GUI/Accessors/AssemblyInformationAccessor.cs
@@ -0,0 +1,106 @@
+﻿using System.Reflection;
+
+namespace AudioRepeaterManager.NET8.GUI.Accessors
+{
+  public class AssemblyInformationAccessor
+  {
+    #region Parameters
+
+    public static string AssemblyCompany
+    {
+      get
+      {
+        object[] attributes = Assembly
+          .GetExecutingAssembly()
+          .GetCustomAttributes(
+            typeof(AssemblyCompanyAttribute),
+            false
+          );
+
+        if (attributes.Length == 0)
+        {
+          return string.Empty;
+        }
+
+        return ((AssemblyCompanyAttribute)attributes[0]).Company;
+      }
+    }
+
+    public static string AssemblyCopyright
+    {
+      get
+      {
+        object[] attributes = Assembly
+          .GetExecutingAssembly()
+          .GetCustomAttributes
+          (
+            typeof(AssemblyCopyrightAttribute),
+            false
+          );
+
+        if (attributes.Length == 0)
+        {
+          return string.Empty;
+        }
+
+        return ((AssemblyCopyrightAttribute)attributes[0]).Copyright;
+      }
+    }
+
+    public static string AssemblyDescription
+    {
+      get
+      {
+        object[] attributes = Assembly
+          .GetExecutingAssembly()
+          .GetCustomAttributes
+          (
+            typeof(AssemblyDescriptionAttribute),
+            false
+          );
+
+        if (attributes.Length == 0)
+        {
+          return string.Empty;
+        }
+
+        return ((AssemblyDescriptionAttribute)attributes[0]).Description;
+      }
+    }
+
+    public static string AssemblyProduct
+    {
+      get
+      {
+        object[] attributes = Assembly
+          .GetExecutingAssembly()
+          .GetCustomAttributes
+          (
+            typeof(AssemblyProductAttribute),
+            false
+          );
+
+        if (attributes.Length == 0)
+        {
+          return string.Empty;
+        }
+
+        return ((AssemblyProductAttribute)attributes[0]).Product;
+      }
+    }
+
+    public static string AssemblyVersion
+    {
+      get
+      {
+        return Assembly
+          .GetExecutingAssembly()
+          .GetName()
+          .Version
+          .ToString();
+      }
+    }
+
+    #endregion
+  }
+}
--- AudioRepeaterManager.NET8/AudioRepeaterManager.NET8_0.Backend/Global.cs
+++ AudioRepeaterManager.NET8/AudioRepeaterManager.NET8_0.Backend/Global.cs
@@ -1,5 +1,10 @@
 ﻿using System;
+using System.Diagnostics;
 using System.IO;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
+using System.Security.Policy;
 
 namespace AudioRepeaterManager.NET8_0.Backend
 {
@@ -10,10 +15,83 @@ namespace AudioRepeaterManager.NET8_0.Backend
   {
     #region Parameters
 
-    #region Application name
+    #region Application info
 
+    public static string ApplicationAbbreviatedName
+    {
+      get
+      {
+        return string.Format
+        (
+          "{0}ARM",
+          ReferencedApplicationName
+        );
+      }
+    }
+
+    public static string ApplicationName
+    {
+      get
+      {
+        return string.Format
+        (
+           "{0} Audio Repeater Manager",
+            ReferencedApplicationName
+        );
+      }
+    }
+
+    public static string ApplicationPartialAbbreviatedName
+    {
+      get
+      {
+        return string.Format
+        (
+           "{0} Audio Repeater Manager",
+            ReferencedApplicationAbbreviatedName
+        );
+      }
+    }
+
+    public readonly static string ReferencedApplicationAbbreviatedName = "VAC";
     public readonly static string ReferencedApplicationName = "Virtual Audio Cable";
     public readonly static string ReferencedFileExtension = ".vac";
+
+    public static string ScriptFileExtension
+    {
+      get
+      {
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+        {
+          return ".bat";
+        }
+
+        else if
+        (
+          RuntimeInformation.IsOSPlatform(OSPlatform.Linux)
+          || RuntimeInformation.IsOSPlatform(OSPlatform.OSX)
+          || RuntimeInformation.IsOSPlatform(OSPlatform.FreeBSD)
+        )
+        {
+          return ".sh";
+        }
+
+        else
+        {
+          throw new NotImplementedException
+            (
+              string.Format
+              (
+                "Failed to determine script file extension."
+                + "Operating System '{0}' is not supported.",
+               RuntimeInformation.OSDescription
+              )
+            );
+        }
+      }
+    }
+
+    public readonly static string XMLFileExtension = ".xml";
 
     #endregion
 
